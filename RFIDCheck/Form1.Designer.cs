a9fcb446717c0afaa07165ab12fe95efcb444ab9
Francesco <runci@pm.me>
2021-03-20 16:25:51 +0100

    add image

--- RFIDCheck/Form1.Designer.cs
+++ RFIDCheck/Form1.Designer.cs
@@ -44,11 +44,13 @@ namespace RFIDCheck
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(50, 227);
+            this.button1.Location = new System.Drawing.Point(261, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(374, 23);
             this.button1.TabIndex = 0;
@@ -58,14 +60,14 @@ namespace RFIDCheck
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(50, 12);
+            this.textBox1.Location = new System.Drawing.Point(261, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(374, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(50, 285);
+            this.textBox2.Location = new System.Drawing.Point(261, 285);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(374, 20);
             this.textBox2.TabIndex = 2;
@@ -73,7 +75,7 @@ namespace RFIDCheck
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 288);
+            this.label1.Location = new System.Drawing.Point(215, 288);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 3;
@@ -82,7 +84,7 @@ namespace RFIDCheck
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 15);
+            this.label2.Location = new System.Drawing.Point(226, 15);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 4;
@@ -91,7 +93,7 @@ namespace RFIDCheck
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 41);
+            this.label3.Location = new System.Drawing.Point(221, 41);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(34, 13);
             this.label3.TabIndex = 6;
@@ -99,14 +101,14 @@ namespace RFIDCheck
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(50, 38);
+            this.textBox3.Location = new System.Drawing.Point(261, 38);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(374, 20);
             this.textBox3.TabIndex = 5;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(50, 90);
+            this.richTextBox1.Location = new System.Drawing.Point(261, 90);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(374, 105);
             this.richTextBox1.TabIndex = 7;
@@ -114,7 +116,7 @@ namespace RFIDCheck
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(230, 256);
+            this.button2.Location = new System.Drawing.Point(441, 256);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(194, 23);
             this.button2.TabIndex = 8;
@@ -124,7 +126,7 @@ namespace RFIDCheck
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(50, 256);
+            this.button3.Location = new System.Drawing.Point(261, 256);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(174, 23);
             this.button3.TabIndex = 10;
@@ -135,14 +137,14 @@ namespace RFIDCheck
             // textBox4
             // 
             this.textBox4.AccessibleName = "";
-            this.textBox4.Location = new System.Drawing.Point(50, 201);
+            this.textBox4.Location = new System.Drawing.Point(261, 201);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(374, 20);
             this.textBox4.TabIndex = 11;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(50, 64);
+            this.textBox5.Location = new System.Drawing.Point(261, 64);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(374, 20);
             this.textBox5.TabIndex = 12;
@@ -150,7 +152,7 @@ namespace RFIDCheck
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 67);
+            this.label4.Location = new System.Drawing.Point(225, 67);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(30, 13);
             this.label4.TabIndex = 13;
@@ -159,7 +161,7 @@ namespace RFIDCheck
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 93);
+            this.label5.Location = new System.Drawing.Point(225, 93);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 14;
@@ -168,17 +170,27 @@ namespace RFIDCheck
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 204);
+            this.label6.Location = new System.Drawing.Point(220, 204);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(37, 13);
             this.label6.TabIndex = 15;
             this.label6.Text = "ASCII:";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RFIDCheck.Properties.Resources.rfid_nfc_reader_uFR1;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 300);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 317);
+            this.ClientSize = new System.Drawing.Size(643, 312);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -197,6 +209,7 @@ namespace RFIDCheck
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RFIDCheck 1.0 by RUNCI Francesco";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,6 +232,7 @@ namespace RFIDCheck
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
