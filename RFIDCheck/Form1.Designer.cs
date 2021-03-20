fd2577a0f0cf1426487bb7da21a02cc31a3bf301
Francesco <runci@pm.me>
2021-03-20 14:26:12 +0100

    nuova implementazione

--- RFIDCheck/Form1.Designer.cs
+++ RFIDCheck/Form1.Designer.cs
@@ -36,13 +36,17 @@ namespace RFIDCheck
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(5, 215);
+            this.button1.Location = new System.Drawing.Point(37, 215);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(406, 23);
+            this.button1.Size = new System.Drawing.Size(374, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Leggi";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,19 +61,19 @@ namespace RFIDCheck
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(48, 244);
+            this.textBox2.Location = new System.Drawing.Point(37, 244);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(363, 20);
+            this.textBox2.Size = new System.Drawing.Size(374, 20);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(2, 247);
+            this.label1.Location = new System.Drawing.Point(12, 247);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(19, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Status:";
+            this.label1.Text = "-->";
             // 
             // label2
             // 
@@ -83,7 +87,7 @@ namespace RFIDCheck
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(2, 41);
+            this.label3.Location = new System.Drawing.Point(-3, 41);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(34, 13);
             this.label3.TabIndex = 6;
@@ -96,11 +100,50 @@ namespace RFIDCheck
             this.textBox3.Size = new System.Drawing.Size(374, 20);
             this.textBox3.TabIndex = 5;
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(37, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(374, 90);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Scrivi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Format";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(37, 160);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(374, 20);
+            this.textBox4.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 266);
+            this.ClientSize = new System.Drawing.Size(414, 271);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
@@ -109,6 +152,7 @@ namespace RFIDCheck
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RFIDCheck 1.0 by RUNCI Francesco";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -124,6 +168,10 @@ namespace RFIDCheck
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- RFIDCheck/Form1.cs
+++ RFIDCheck/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Windows.Forms;
 using uFR;
 
@@ -7,33 +8,13 @@ namespace RFIDCheck
 
     public partial class Form1 : Form
     {
+        DL_STATUS status = DL_STATUS.UFR_READER_PORT_NOT_OPENED;
+
 
         public Form1()
         {
             InitializeComponent();
         }
-
-        const byte DL_MIFARE_ULTRALIGHT = 0x01,
-                  DL_MIFARE_ULTRALIGHT_EV1_11 = 0x02,
-                  DL_MIFARE_ULTRALIGHT_EV1_21 = 0x03,
-                  DL_MIFARE_ULTRALIGHT_C = 0x04,
-                  DL_NTAG_203 = 0x05,
-                  DL_NTAG_210 = 0x06,
-                  DL_NTAG_212 = 0x07,
-                  DL_NTAG_213 = 0x08,
-                  DL_NTAG_215 = 0x09,
-                  DL_NTAG_216 = 0x0A,
-                  DL_MIFARE_MINI = 0x20,
-                  DL_MIFARE_CLASSIC_1K = 0x21,
-                  DL_MIFARE_CLASSIC_4K = 0x22,
-                  DL_MIFARE_PLUS_S_2K = 0x23,
-                  DL_MIFARE_PLUS_S_4K = 0x24,
-                  DL_MIFARE_PLUS_X_2K = 0x25,
-                  DL_MIFARE_PLUS_X_4K = 0x26,
-                  DL_MIFARE_DESFIRE = 0x27,
-                  DL_MIFARE_DESFIRE_EV1_2K = 0x28,
-                  DL_MIFARE_DESFIRE_EV1_4K = 0x29,
-                  DL_MIFARE_DESFIRE_EV1_8K = 0x2A;
         String GetDlTypeName(byte dl_type_code)
         {
             String s;
@@ -98,14 +79,218 @@ namespace RFIDCheck
 
             return s;
         }
+        const byte DL_MIFARE_ULTRALIGHT = 0x01,
+                  DL_MIFARE_ULTRALIGHT_EV1_11 = 0x02,
+                  DL_MIFARE_ULTRALIGHT_EV1_21 = 0x03,
+                  DL_MIFARE_ULTRALIGHT_C = 0x04,
+                  DL_NTAG_203 = 0x05,
+                  DL_NTAG_210 = 0x06,
+                  DL_NTAG_212 = 0x07,
+                  DL_NTAG_213 = 0x08,
+                  DL_NTAG_215 = 0x09,
+                  DL_NTAG_216 = 0x0A,
+                  DL_MIFARE_MINI = 0x20,
+                  DL_MIFARE_CLASSIC_1K = 0x21,
+                  DL_MIFARE_CLASSIC_4K = 0x22,
+                  DL_MIFARE_PLUS_S_2K = 0x23,
+                  DL_MIFARE_PLUS_S_4K = 0x24,
+                  DL_MIFARE_PLUS_X_2K = 0x25,
+                  DL_MIFARE_PLUS_X_4K = 0x26,
+                  DL_MIFARE_DESFIRE = 0x27,
+                  DL_MIFARE_DESFIRE_EV1_2K = 0x28,
+                  DL_MIFARE_DESFIRE_EV1_4K = 0x29,
+                  DL_MIFARE_DESFIRE_EV1_8K = 0x2A;
+
+        public static byte[] ToByteArray(String HexString)
+        {
+
+            int NumberChars = HexString.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+
+            if (HexString.Length % 2 != 0)
+            {
+                return bytes;
+            }
+
+            for (int i = 0; i < NumberChars; i += 2)
+            {
+                try
+                {
+                    bytes[i / 2] = Convert.ToByte(HexString.Substring(i, 2), 16);
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Incorrect format!");
+                    break;
+                }
+            }
+
+            return bytes;
+        }
+
+
+        public static string ConvertHex(String hexString)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    String hs = string.Empty;
+
+                    hs = hexString.Substring(i, 2);
+                    uint decval = System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+
+                }
+
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
+        }
+
+        public static string AsciiToHex(string asciiString)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char c in asciiString)
+            {
+                builder.Append(Convert.ToInt32(c).ToString("X"));
+            }
+            return builder.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox2.Clear();
+            richTextBox1.Clear();
+
+            if (status == DL_STATUS.UFR_OK)
+            {
+                byte[] LWData = new byte[2048];
+                String LinAddr = "0";
+                String LWData_Str = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+                ushort linear_address = 0;
+
+
+                try
+                {
+                    linear_address = ushort.Parse(LinAddr);
+
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Incorect start byte!");
+
+                }
+
+                ushort byte_written = 0;
+                LWData = ToByteArray(LWData_Str);
+                Int32 data_len = LWData_Str.Length / 2;
+
+                status = uFCoder.LinearWrite(LWData, linear_address, (ushort)data_len, ref byte_written, 0x60, 0);
+
+                textBox2.Text = uFCoder.status2str(status);
+                MessageBox.Show("Formattazione Eseguita!");
+
+            }
+            else
+            {
+                MessageBox.Show("Scheda non pronta!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox2.Clear();
+
+            if (status == DL_STATUS.UFR_OK)
+            {
+                byte[] LWData = new byte[2048];
+                String LinAddr = "0";
+                String LWData_Str = AsciiToHex(textBox4.Text.ToString());
+
+                ushort linear_address = 0;
+
+
+                try
+                {
+                    linear_address = ushort.Parse(LinAddr);
+
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Incorect start byte!");
+
+                }
+
+                ushort byte_written = 0;
+                LWData = ToByteArray(LWData_Str);
+                Int32 data_len = LWData_Str.Length / 2;
+
+                status = uFCoder.LinearWrite(LWData, linear_address, (ushort)data_len, ref byte_written, 0x60, 0);
+
+                textBox2.Text = uFCoder.status2str(status);
+                MessageBox.Show("Lettura Eseguita!");
+
+            }
+            else
+            {
+                MessageBox.Show("Scheda non pronta!");
+            }
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             byte CardType = 0;
             byte[] Uid = new byte[10];
             byte UidSize = 0;
 
-            DL_STATUS status = DL_STATUS.UFR_READER_PORT_NOT_OPENED;
+            byte[] LinearData = new byte[2048];
+            String LinAddr = "0";
+            String Len = "100";
+            ushort linear_address = 0;
+            ushort data_len = 0;
+
+            try
+            {
+
+                linear_address = ushort.Parse(LinAddr);
+
+            }
+            catch (System.FormatException)
+            {
+
+                MessageBox.Show("Incorect start byte!");
+                textBox2.Clear();
+            }
+
+            try
+            {
+                data_len = ushort.Parse(Len);
+
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Incorect length!");
+                textBox2.Clear();
+            }
+
+
+            ushort bytes_written = 0;
+
             status = uFCoder.ReaderOpen();
 
             status = uFCoder.GetCardIdEx(ref CardType, Uid, ref UidSize);
@@ -115,6 +300,24 @@ namespace RFIDCheck
                 textBox2.Text = uFCoder.status2str(status);
                 textBox1.Text = "[" + BitConverter.ToString(Uid).Replace("-", ":") + "]";
                 textBox3.Text = "[" + GetDlTypeName(CardType) + "]";
+                status = uFCoder.LinearRead(LinearData, linear_address, data_len, ref bytes_written, 0x60, 0);
+
+                textBox2.Text = uFCoder.status2str(status);
+                if (status == 0)
+                {
+
+                    byte[] LinearShow = new byte[data_len];
+
+                    Array.Copy(LinearData, LinearShow, data_len);
+                    
+                    richTextBox1.Text = BitConverter.ToString(LinearShow).Replace("-", ":");
+                    textBox4.Text = ConvertHex(BitConverter.ToString(LinearShow).Replace("-", ""));
+
+                }
+                else
+                {
+                    richTextBox1.Clear();
+                }
 
             }
             else
@@ -122,11 +325,13 @@ namespace RFIDCheck
                 textBox2.Text = "Errore lettura.";
                 textBox1.Clear();
                 textBox3.Clear();
+                textBox4.Clear();
 
             }
 
 
         }
+
 
     }
 }
